34413d45597742bda9a6eee04e7cc2eb6a3bc29e
pandeyneha0123 <pandeyneha0123>
2023-04-24 19:48:53 +0530

    add

---
+++ yebeloAssignment/src/main/java/com/model/NumberResponse.java
@@ -0,0 +1,28 @@
+package com.model;
+
+public class NumberResponse {
+    private int oldValue;
+    private int newValue;
+
+    public NumberResponse(int oldValue, int newValue) {
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+    }
+
+    public int getOldValue() {
+        return oldValue;
+    }
+
+    public void setOldValue(int oldValue) {
+        this.oldValue = oldValue;
+    }
+
+    public int getNewValue() {
+        return newValue;
+    }
+
+    public void setNewValue(int newValue) {
+        this.newValue = newValue;
+    }
+}
+
