7ef44c135a2ac731f0bafe7de7ee8901d7ed0615
pandeyneha0123 <pandeyneha0123>
2023-04-24 19:01:33 +0530

    add

---
+++ yebeloAssignment/src/main/java/com/yebelo/YebeloAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.yebelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YebeloAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YebeloAssignmentApplication.class, args);
+	}
+
+}
---
+++ yebeloAssignment/src/test/java/com/yebelo/YebeloAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yebelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YebeloAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
